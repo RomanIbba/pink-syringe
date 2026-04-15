d91c96c2112ec78494d525c5f7e98304ee23cff6
Roman <romanibba95@gmail.com>
2026-04-14 17:15:18 -0700

    Configure GitHub Pages with Docs

--- docs/_next/static/bukQEA-zeRtf0_fU0ZvdU/_buildManifest.js
+++ docs/_next/static/bukQEA-zeRtf0_fU0ZvdU/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pink-syringe//_next/:path+",
+        "destination": "/pink-syringe/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
